8e600568f77eeeb1ca6731f8e5ef93e2c8ced171
Alexander Kabui <41166763+Alexanderlacuna@users.noreply.github.com>
2020-02-22 11:57:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+let fruits=["oranges","mango","cassava","apples"]
+
+let index=fruits.findIndex((item)=>item=="cassava")
+let indexof=fruits.indexOf((item)=>item=="cassava")
+index 
+indexof 
+
+let date=Date.now()
+console.log(typeof(date))
+
+
+let timer=document.querySelector(".Timer")
+
+
+var d= new Date("2020-02-26 11:10");
+
+let d2=date/(1000*60*60*60)
+
+d2
+let c=d.getTime()
+
+console.log(c)
+
+
+let d3=c-date
+
+d3/(1000*60*60*60*60)
+console.log(d3/(1000*60*60*60))
+
+hours=d3%(1000*60*60*24)/(1000*60*60)
+console.log(hours)
+
+// minutes=d3%(1000*60*60)/
+// console.log(c-date)
+ var date2 = new Date("2020/12/25 00:00:00");
+ var date1=new Date()
+
+ 
+function getDifference(){
+
+	 var date1=new Date()
+
+
+	var diff=(date2-date1)/1000
+	// console.log(diff)
+	diff=Math.abs(Math.floor(diff))
+	var days=Math.floor(diff/(24*60*60))
+
+	days 
+	var leftSec=diff-days*24*60*60
+	var hrs=Math.floor(leftSec/(60*60))
+
+	hrs 
+	var leftSec=leftSec-(hrs*60*60)
+	var minutes=Math.floor(leftSec/60)
+	minutes
+	var  leftSec=leftSec-(minutes*60)
+
+	var seconds=leftSec<10? `0${leftSec}` :`${leftSec}`
+
+	minutes=minutes<10? `0${minutes}` :`${minutes}`
+
+	// leftSec
+
+	// console.log(timer) 
+
+
+	timer.innerHTML=`<span  class="days">Days:${days} </span><span class="hours">Hours:${hrs}</span><span class="minutes"> Minutes:${minutes} </span><span class="seconds">Seconds ${seconds}</span>` 
+
+}
+
+// getDifference(date1, date2)
+let count=setInterval(getDifference,1000)
+
+
+
+// count
